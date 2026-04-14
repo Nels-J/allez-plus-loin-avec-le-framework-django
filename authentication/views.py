bbaa0a382376dc3a2449161b661b5a691110065c
Nelson Jesus <n.jesus@tracknco.com>
2026-04-14 17:41:49 +0200

    refactor login_page view to improve request handling and add comments

--- authentication/views.py
+++ authentication/views.py
@@ -6,20 +6,23 @@ from . import forms
 
 
 def login_page(request):
+    # Gestion de la requete par défaut - GET
     form = forms.LoginForm()
     message = ""
+    # Gestion de la requete POST
     if request.method == "POST":
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            user = authenticate(
+            user = authenticate(  # Vérifie l'authenticité du couple nom/pwd
                 username=form.cleaned_data["username"],
                 password=form.cleaned_data["password"],
             )
             if user is not None:
-                login(request, user)
+                login(request, user)  # Gère la connexion de l'utilisateur authentifié à la requête
                 message = f"Bonjour, {user.username}! Vous êtes connecté."
             else:
                 message = "Identifiants invalides."
+    # Retour en cas de GET ou POST sans erreur
     return render(
         request, "authentication/login.html", context={"form": form, "message": message}
     )
