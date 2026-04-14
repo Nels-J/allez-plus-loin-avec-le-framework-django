f3ee45913117417ad3a6ab177eb053059502c16c
Nelson Jesus <n.jesus@tracknco.com>
2026-04-14 18:09:28 +0200

    add logout functionality and update base template with user authentication status

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,13 @@
 # authentication/views.py
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 from . import forms
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
 
 
 def login_page(request):
