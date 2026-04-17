f7912870be8f0cf1f968c68a23dc7c5b70679adc
Nelson Jesus <n.jesus@tracknco.com>
2026-04-17 11:41:33 +0200

    update login view to use Django's built-in LoginView and set LOGIN_REDIRECT_URL

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 import authentication.views
@@ -22,7 +23,10 @@ import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.LoginPage.as_view(), name='login'),
+    path('', LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True,  # Utilise LOGIN_REDIRECT_URL dans settings.py
+    ), name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
 ]
